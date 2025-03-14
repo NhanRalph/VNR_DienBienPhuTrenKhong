7b58d354f9ecffb288ab8907e91d7e2834f421fc
Dao Nguyen Huy Nhan <dnhnhan2003@gmail.com>
2025-03-14 15:37:53 +0700

    fix vid

--- src/components/BattleVideos.jsx
+++ src/components/BattleVideos.jsx
@@ -4,8 +4,6 @@ const videos = [
   { id: "t_j0truPdrw", title: "Điện Biên Phủ Trên Không - Tư Liệu Lịch Sử" },
   { id: "EZAb6IBguSc", title: "Chiến Dịch 12 Ngày Đêm - Hủy Diệt B-52" },
   { id: "wL8J9VSIQcc", title: "PHÓNG SỰ TÀI LIỆU: CHIẾN THẮNG HÀ NỘI - ĐIỆN BIÊN PHỦ" },
-  { id: "kILCkBij2b8", title: "Pháp Đã ĐẠI BẠI Ở Điện Biên Phủ Như Thế Nào?" },
-  { id: "CD8sKixEDsI", title: "Tóm tắt nhanh Chiến Dịch Điện Biên Phủ (1954)" },
 ];
 
 export default function BattleVideos() {
